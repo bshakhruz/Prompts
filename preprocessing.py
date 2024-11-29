836742df9b8af8999275b058d8acabeb98507304
bshakhruz <baxtiyorovshaxruzjon@gmail.com>
2024-11-29 17:39:35 +0500

    Preprocessor agent

---
+++ preprocessing.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Name: AI Preprocessor
+Description: Performs 
+Author:
+"""
+import ollama
+
+{
+    "role": "system",
+    "content": f"""You are text preprocessor.
+    **Task**
+    - perform preprocessing on all steps over texts.
+    - minimaze tokens and garbages
+    """
+}
