f7c8e9c1c558da11282b1652740f06a31e291a4c
bshakhruz <baxtiyorovshaxruzjon@gmail.com>
2024-11-29 17:57:15 +0500

    updated the script

--- preprocessing.py
+++ preprocessing.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 """
 Name: AI Preprocessor
-Description: Performs 
-Author:
+Description: Performs preprocessing over text data types.
+Author: bshakhruz
 """
 import ollama
 
-{
+def system_instruction():
+    """Preprocess texts"""
+    return {
     "role": "system",
-    "content": f"""You are text preprocessor.
-    **Task**
-    - perform preprocessing on all steps over texts.
-    - minimaze tokens and garbages
-    """
-}
+    "content": f"""You are text preprocessor.\n
+    **Task**\n
+    - perform preprocessing on all steps over texts.\n
+    - minimaze tokens and garbages\n
+    """,
+    #TO-DO: pass additional params
+            }
